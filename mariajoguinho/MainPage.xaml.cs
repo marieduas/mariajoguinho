f8181dd8c35735760b6a423b7e738d637028a9b2
mariajoguinho <malvesdasi82gmail.com>
2024-04-05 10:59:15 -0300

    003

--- mariajoguinho/MainPage.xaml.cs
+++ mariajoguinho/MainPage.xaml.cs
@@ -6,6 +6,33 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
+	void AtualizaPersonagem()
+  {
+	progressoFome.Progress=Atual.GetFome();
+	progressoSede.Progress=Atual.GetSede();
+    progressoAlegria.Progress=Atual.GetAlegria();
+  }
+void BotaoComidaFoiClicado(object sender, EventArgs args)
+  {
+	Atual.SetFome(Atual.GetFome()+0,1);
+	AtualizaPersonagem(); 
+  }
+void BotaoAguaFoiCLicado(object sender, EventArgs args)
+  {
+    Atual.SetSede(Atual.GetSede()+0,1);
+	AtualizaPersonagem();
+  }
+void BotaoBrinquedoFoiClicado(object sender, EventArgs args)
+  {
+	Atual.SetAlegria(Atual.GetAlegria()+0,1);
+	AtualizaPersonagem();
+  }  
+void PassouTempo()
+  {
+	Atual.SetFome(Atual.GetFome()+0,1);
+	Atual.SetSede(Atual.GetSede()+0,1);
+    Atual.SetAlegria(Atual.GetAlegria()+0,1);
+    AtualizaPersonagem();
+  }  
 }
 
